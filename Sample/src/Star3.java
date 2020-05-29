ff93aa4e99bdd8c76aeeec3ded1c77d21774cbaf
admin <admin@DESKTOP-O1I6GR6>
2020-05-29 17:18:39 +0900

    star3 문제

---
+++ Sample/src/Star3.java
@@ -0,0 +1,18 @@
+
+public class Star3 {
+	public static void main(String[] args) {
+		int center = 5 / 2 + 1 ;
+		int space = 0;
+		for(int row = 1 ; row <= 10 ; row++) {
+			center = 10 - row;
+			System.out.print(" ");
+		}
+			
+		
+		if(space == 5) {
+		
+				System.out.print("*");
+			}
+	}
+
+}
